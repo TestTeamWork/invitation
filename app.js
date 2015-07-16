8099a90bcb28802ef38e5a8de25e014922ac5e53
yancey <yancey1204@gmail.com>
2015-07-16 02:26:40 +0200

    add the animation of background with style in lib/style.css & add the animated heart & add the animation of typing for the text

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
 		scrollingSpeed:1000,
 		css3: true
 	});
-
+/*
 	$('.up').velocity(
 	{translateY:["3em","4.5em"] },
 	{
 		duration: 1700 ,
     	loop: true
 	});
-		
+		*/
 	$('.down').velocity(
 	{translateY: ["1.5em","0em"] },	
 	{
@@ -34,7 +34,37 @@ $(document).ready(function() {
     	loop: true
 	});
 		
-	
+
+// the animation of the heart
+$('.inside').each(function(id){
+    $(this).css({
+      position: 'relative',
+      top: '-200px',
+      opacity: 0
+    });
+    var wait = Math.floor((Math.random()*3000)+1);
+    $(this).delay(wait).animate({
+      top: '0px',
+      opacity: 1
+    },1000);
+  });
+
+// animation for the text
+$(".text").typed({
+        strings: ["我们结婚啦"],
+        typeSpeed:350,
+        backDelay: 1000,
+        loop: false,
+        loopCount: false,
+      });
+$(".text2").typed({
+        strings: ["时间：2015年9月5号， 周六^500","地点：滨海建国大酒店^500","交通：天津滨海建国大酒店距离滨海国际机场38公里，您可以选择机场大巴或者出租车前往酒店。如果您乘坐机场大巴，请在滨海国际机场国内出口处到达厅乘前往天津开发区的大巴车，车票25元。抵达泰达中心站，然后搭乘出租车约9元到达酒店；如果您选择出租车，从滨海国际机场到酒店约170元车费（含20元高速费），请别忘了放在车上的行李。"],
+        typeSpeed:150,
+        backDelay: 1000,
+        loop: false,
+        loopCount: false,
+      });
+
 	// add a map
 	var map = new AMap.Map('mapContainer', {
 		resizeEnable: true,
